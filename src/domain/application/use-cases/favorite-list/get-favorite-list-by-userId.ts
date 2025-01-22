1daee4a7e8d2a82da0ca02f2360c5b2ef9d8f6f8
Guilherme de Almeida <guilherme.amf18@gmail.com>
2025-01-22 11:08:41 -0300

    feat: get favorite list by Userid use case

---
+++ src/domain/application/use-cases/favorite-list/get-favorite-list-by-userId.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { FavoriteList } from 'src/domain/enterprise/entities/favorite-list';
+import { UniqueEntityID } from 'src/core/entity/unique-entity-id';
+import { EntityNotFoundError } from 'src/core/errors/commom/entity-not-found-error';
+import { FavoriteListRepository } from '../../repositories/favorite-list-repository.interface';
+
+export type GetFavoriteListByUserIdRequest = {
+  userId: string;
+};
+
+export type GetFavoriteListByIdResponse = FavoriteList;
+
+@Injectable()
+export class GetFavoriteListByIdUseCase {
+  constructor(private readonly favoriteListRepo: FavoriteListRepository) {}
+
+  async execute(
+    request: GetFavoriteListByUserIdRequest,
+  ): Promise<GetFavoriteListByIdResponse> {
+    try {
+      const entityId = new UniqueEntityID(request.userId);
+
+      const favoriteList = await this.favoriteListRepo.findById(entityId);
+
+      if (!favoriteList) {
+        throw new EntityNotFoundError('FavoriteList', request.userId);
+      }
+
+      return favoriteList;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
