41ebc903f115f78b6f2a05a67263c9c78ee9a0a4
Guilherme de Almeida <guilherme.amf18@gmail.com>
2025-01-22 11:13:47 -0300

    feat: add http module

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { CreateFavoriteListUseCase } from 'src/domain/application/use-cases/favorite-list/create-favorite-list';
+import { GetFavoriteListByIdUseCase } from 'src/domain/application/use-cases/favorite-list/get-favorite-list-by-userId';
+import { DatabaseModule } from '../database/database.module';
+import { UpdateFavoriteListUseCase } from 'src/domain/application/use-cases/favorite-list/update-favorite-list';
+import { DeleteFavoriteListUseCase } from 'src/domain/application/use-cases/favorite-list/delete-favorite-list';
+import { AuthCustomerUseCase } from 'src/domain/application/use-cases/customer/auth-customer';
+import { RegisterCustomerUseCase } from 'src/domain/application/use-cases/customer/register-customer';
+import { FavoriteProductUseCase } from 'src/domain/application/use-cases/favorite-list/favorite-product';
+import { UnFavoriteProductUseCase } from 'src/domain/application/use-cases/favorite-list/unfavorite-product';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [],
+  providers: [
+    CreateFavoriteListUseCase,
+    GetFavoriteListByIdUseCase,
+    UpdateFavoriteListUseCase,
+    DeleteFavoriteListUseCase,
+    AuthCustomerUseCase,
+    RegisterCustomerUseCase,
+    FavoriteProductUseCase,
+    UnFavoriteProductUseCase,
+  ],
+})
+export class HttpModule {}
