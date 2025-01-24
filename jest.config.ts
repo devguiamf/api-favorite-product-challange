fa29e34985bd93455d17ccefcae0a9f674dc7eae
Guilherme de Almeida <guilherme.amf18@gmail.com>
2025-01-24 15:54:37 -0300

    fix: fixing jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+  modulePaths: ['<rootDir>/src'],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts'],
+};
