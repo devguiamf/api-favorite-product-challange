5c6dcb0719b94bdf6cbf7e63b43fd68ac7f43f87
Guilherme de Almeida <guilherme.amf18@gmail.com>
2025-01-28 13:53:20 -0300

    test: create a favorite list test

--- src/domain/application/use-cases/favorite-list/__tests__/create-favorite-list.spec.ts
+++ src/domain/application/use-cases/favorite-list/__tests__/create-favorite-list.spec.ts
@@ -1,11 +1,16 @@
-import { faker } from "@faker-js/faker/.";
-import { CreateFavoriteListRequest, CreateFavoriteListUseCase } from "../create-favorite-list";
-import { UniqueEntityID } from "src/core/entity/unique-entity-id";
-import { FavoriteListRepository } from "src/domain/application/repositories/favorite-list-repository.interface";
-import { mock } from "jest-mock-extended";
-import { FavoriteList } from "src/domain/enterprise/entities/favorite-list";
+import { faker } from '@faker-js/faker/.';
+import {
+  CreateFavoriteListRequest,
+  CreateFavoriteListUseCase,
+} from '../create-favorite-list';
+import { UniqueEntityID } from 'src/core/entity/unique-entity-id';
+import { FavoriteListRepository } from 'src/domain/application/repositories/favorite-list-repository.interface';
+import { mock } from 'jest-mock-extended';
+import { FavoriteList } from 'src/domain/enterprise/entities/favorite-list';
 
-function makeCreateFavoriteListRequest(modifications?: Partial<CreateFavoriteListRequest>): CreateFavoriteListRequest {
+function makeCreateFavoriteListRequest(
+  modifications?: Partial<CreateFavoriteListRequest>,
+): CreateFavoriteListRequest {
   return {
     title: faker.lorem.words(3),
     description: faker.lorem.sentence(),
@@ -15,37 +20,38 @@ function makeCreateFavoriteListRequest(modifications?: Partial<CreateFavoriteLis
 }
 
 describe(CreateFavoriteListUseCase.name, () => {
-    let sut: CreateFavoriteListUseCase;
-    let favoriteListRepo: jest.Mocked<FavoriteListRepository>;
+  let sut: CreateFavoriteListUseCase;
+  let favoriteListRepo: jest.Mocked<FavoriteListRepository>;
 
-    beforeEach(() => {
-        favoriteListRepo = mock<FavoriteListRepository>();
-        sut = new CreateFavoriteListUseCase(favoriteListRepo);
-    });
+  beforeEach(() => {
+    favoriteListRepo = mock<FavoriteListRepository>();
+    sut = new CreateFavoriteListUseCase(favoriteListRepo);
+  });
 
-    it('should create a new list favorite', async () => {
-        const createFavoriteListRequest = makeCreateFavoriteListRequest();
+  it('should create a new list favorite', async () => {
+    const createFavoriteListRequest = makeCreateFavoriteListRequest();
 
-        favoriteListRepo.exists.mockResolvedValueOnce(false);
+    favoriteListRepo.exists.mockResolvedValueOnce(false);
 
-        const result = await sut.execute(createFavoriteListRequest);
+    const result = await sut.execute(createFavoriteListRequest);
 
-        expect(result).toBeDefined();
-        expect(result).toBeInstanceOf(FavoriteList);
-        expect(result.title).toBe(createFavoriteListRequest.title);
-        expect(result.description).toBe(createFavoriteListRequest.description);
-        expect(result.userId).toBe(createFavoriteListRequest.userId);
-        expect(result.products).toEqual([]);
-        expect(favoriteListRepo.save).toHaveBeenCalled();
-        expect(favoriteListRepo.exists).toHaveBeenCalled();
+    expect(result).toBeDefined();
+    expect(result).toBeInstanceOf(FavoriteList);
+    expect(result.title).toBe(createFavoriteListRequest.title);
+    expect(result.description).toBe(createFavoriteListRequest.description);
+    expect(result.userId).toBe(createFavoriteListRequest.userId);
+    expect(result.products).toEqual([]);
+    expect(favoriteListRepo.save).toHaveBeenCalled();
+    expect(favoriteListRepo.exists).toHaveBeenCalled();
+  });
 
-    });
+  it('should throw an error when the list favorite already exists', async () => {
+    const createFavoriteListRequest = makeCreateFavoriteListRequest();
 
-    it('should throw an error when the list favorite already exists', async () => {
-        const createFavoriteListRequest = makeCreateFavoriteListRequest();
+    favoriteListRepo.exists.mockResolvedValueOnce(true);
 
-        favoriteListRepo.exists.mockResolvedValueOnce(true);
-
-        await expect(sut.execute(createFavoriteListRequest)).rejects.toThrow('Já existe uma lista de favoritos para este usuário');
-    });
+    await expect(sut.execute(createFavoriteListRequest)).rejects.toThrow(
+      'Já existe uma lista de favoritos para este usuário',
+    );
+  });
 });
